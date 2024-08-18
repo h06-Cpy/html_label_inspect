66c282c95ee443f4fce7d352a2b5bb7dd233cf8f
h06-Cpy <hyjun0103@gmail.com>
2024-08-18 22:44:15 +0900

    [BREAKING CHANGE] 렌더링된 테이블 생성으로 방향 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,76 @@
-import React, {  useState, useRef } from 'react'
+import React, {  useState, useRef, useEffect } from 'react'
 import Editor, {  OnChange } from '@monaco-editor/react';
 import { getLabelInfo, saveLabel } from './api';
-import { BoolTypeQuestion } from './components/BoolTypeQuestion';
-import { ListTypeQuestion } from './components/ListTypeQuestion';
+import html2canvas from 'html2canvas';
 
 function App() {
   const [labelId, setLabelId] = useState(0)
   const [imageName, setImageName] = useState('')
+  const [previewb64, setPreviewb64] = useState('')
+  const previewImageRef = useRef<HTMLImageElement>(null);
   const originImgRef = useRef<HTMLImageElement>(null);
   
-  const [html, setHtml] = useState('')
-
-  const [structCorrect, setStruectCorrect] = useState(false)
-  const [charCorrect, setCharCorrect] = useState(false)
-  const [thUsed, setThUsed] = useState(true)
-  const [valueEmptyCell, setValueEmptyCell] = useState(false)
-  const [specialChar, setSpecialChar] = useState<Set<number>>(new Set([0]))
-  const [cellSubtitle, setCellSubtile] = useState<Set<number>>(new Set([0]))
-  const [semanticMergedCell, setSemanticMergedCell] = useState<Set<number>>(new Set([0]))  
-  const [partialLined, setPartialLined] = useState<Set<number>>(new Set([0]))
-  const [topleftHeader, setTopleftHeader] = useState<Set<number>>(new Set([0]))
-
-  const [isInspected, setIsInspected] = useState(false)
+  const [html, setHtml] = useState('') // 사용자가 입력하는 html
 
   const handleEditorChange: OnChange = (value) => {
     if (value) setHtml(value)
-    else setHtml('')
+
+    else setHtml('') 
   }
+  
+  useEffect(() => { 
+
+    // 브라우저 성능을 위한 debouncing
+    const handler = setTimeout(showPreviewImage, 400);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [html])
+
+  const [divHeight, setDivHeight] = useState('fit')
+  const [divWidth, setDivWidth] = useState('fit')
+  const [divPadding, setDivPadding] = useState(2)
+  const [divFont, setDivFont] = useState('')
+  const [divFontSize, setDivFontSize] = useState('')
+  const [divFontWeight, setDivFontWeight] = useState('')
+
+  useEffect(() => {
+    showPreviewImage();
+  }, [divHeight, divWidth, divFont, divFontSize, divFontWeight, divPadding])
+
+
+  const showPreviewImage = async () => {
+    const renderedTableDOM = document.querySelector("#rendered-table") as HTMLElement | null
+
+    if (renderedTableDOM) {
+      const canvas = await html2canvas(renderedTableDOM)
+      const imageFile = canvas.toDataURL("image/png", 2)
+
+      if (previewImageRef.current) {
+        previewImageRef.current.src = imageFile
+        setPreviewb64(imageFile);
+      }
+    } 
+  }
+
+  const [tdPadding, setTdPadding] = useState(0)
+  const [tableHeight, setTableHeight] = useState('')
+  const [tableWidth, setTableWidth] = useState('')
+  const [tableBorder, setTableBorder] = useState(1)
+
+  const applyStyleToHtml = (html: string) => {
+    return html.replace(/<table(.*?)>/g, `<table style="height: ${tableHeight}vh; width: ${tableWidth}vw; border: ${tableBorder}px solid black;">`)
+                           .replace(/<th(.*?)>/g, `<th style="border: ${tableBorder}px solid black; padding: ${tdPadding}em;">`)
+                           .replace(/<td(.*?)>/g, `<td style="border: ${tableBorder}px solid black; padding: ${tdPadding}em;">`)
+  }
+
+  useEffect(() => {
+    setHtml(applyStyleToHtml(html))
+  }, [tdPadding, tableHeight, tableWidth, tableBorder])
+
+  const [isInspected, setIsInspected] = useState(false)
+  
 
   const fillLabelInfo = async (labelId: number) => {
     const response = await getLabelInfo(labelId)
@@ -41,16 +85,6 @@ function App() {
           originImgRef.current.src = `data:image/png;base64,${response.originImage}`;
         }
 
-        setStruectCorrect(response.structCorrect)
-        setCharCorrect(response.charCorrect)
-        setThUsed(response.thUsed)
-        setValueEmptyCell(response.valueEmptyCell)
-        setSpecialChar(new Set(response.specialChar))
-        setCellSubtile(new Set(response.cellSubtitle))
-        setSemanticMergedCell(new Set(response.semanticMergedCell))
-        setPartialLined(new Set(response.partialLined))
-        setTopleftHeader(new Set(response.topleftHeader))
-
     } else { // 검수 안 한 경우 레이블 html 제공
         setIsInspected(false)
 
@@ -61,15 +95,6 @@ function App() {
           originImgRef.current.src = `data:image/png;base64,${response.originImage}`;
         }
 
-        setStruectCorrect(false)
-        setCharCorrect(false)
-        setThUsed(true)
-        setValueEmptyCell(false)
-        setSpecialChar(new Set([0]))
-        setCellSubtile(new Set([0]))
-        setSemanticMergedCell(new Set([0]))
-        setPartialLined(new Set([0]))
-        setTopleftHeader(new Set([0]))
     }
   }
 
@@ -114,72 +139,149 @@ function App() {
 
       <hr className='border-gray-500'/>
 
-      {/* 에디터 + 원본 + 렌더링된 테이블 부분 */}
-      <div className='mt-5 flex flex-row w-full h-full'>
+      {/* 2 column */}
+      <div className='my-5 flex flex-row w-full h-full'>
 
-        {/* 에디터 */}
-        <div className=' w-1/2 mx-1 border-r-2'>
-        <Editor
-        // width={'85vh'}
-        height={'85vh'}
-        theme='vs-dark'
-        language='html'
-        value={html}
-        onChange={handleEditorChange} />
+        {/* 왼쪽 */}
+        <div className='w-1/2 mx-1 border-r-2'>
+          {/* 에디터 */}
+          <Editor
+          // width={'85vh'}
+          height={'85vh'}
+          theme='vs-dark'
+          language='html'
+          value={html}
+          onChange={handleEditorChange} />
+    
+        <div className='grid grid-cols-2'>
+          {/* div 조절 */}
+          <div className="mx-5 grid grid-rows-6 gap-3">
+            
+            <div>
+            <label htmlFor="div-height" className='mr-5'>div 높이</label>
+            <input name='div-height' type="text" className='p-2' value={divHeight} onChange={(event) => setDivHeight(event.target.value)}/>
+            </div>
+
+            <div>
+            <label htmlFor="div-width" className='mr-5'>div 너비</label>
+            <input name='div-width' type="text" value={divWidth} className='p-2' onChange={(event) => setDivWidth(event.target.value)}/>
+            </div>
+
+            <div>
+            <label htmlFor="div-padding" className='mr-5'>div 패딩</label>
+            <input name='div-padding' type="number" value={divPadding} className='p-2' onChange={(event) => setDivPadding(parseInt(event.target.value))}/>
+            </div>
+
+            <div>
+            <label htmlFor="font-weight" className='mr-5'>폰트 굵기</label>
+            <select name="font-weight" id="font-weight" className='rounded-md p-2' onChange={(event) => setDivFontWeight(event.target.value)}>
+              <option value="thin">thin</option>
+              <option value="extralight">extralight</option>
+              <option value="light">light</option>
+              <option value="normal" selected>normal</option>
+              <option value="medium">medium</option>
+              <option value="semibold">semibold</option>
+              <option value="bold">bold</option>
+              <option value="extrabold">extrabold</option>
+              <option value="black">black</option>
+            </select>
+            </div>
+
+            <div>
+            <label htmlFor="fonts" className='mr-5'>폰트 종류</label>
+            <select name="fonts" id="fonts" className='rounded-md p-2' onChange={(event) => setDivFont(event.target.value)}>
+              <option value="sans">sans</option>
+              <option value="serif">serif</option>
+              <option value="mono" selected>mono</option>
+    
+            </select>
+            </div>
+
+            <div>
+            <label htmlFor="font-size" className='mr-5'>폰트 크기</label>
+            <select name="font-size" id="font-size" className='rounded-md p-2' onChange={(event) => setDivFontSize(event.target.value)}>
+              <option value="xs">xs</option>
+              <option value="sm">sm</option>
+              <option value="base" selected>base</option>
+              <option value="lg">lg</option>
+              <option value="xl">xl</option>
+              <option value="2xl">2xl</option>
+              <option value="3xl">3xl</option>
+              <option value="4xl">4xl</option>
+              <option value="5xl">5xl</option>
+              <option value="6xl">6xl</option>
+            </select>
+            </div>
+          </div>
+
+          {/* table 조절 */}
+          <div className="mx-5 grid grid-rows-4 gap-3">
+            
+            <div>
+            <label htmlFor="table-height" className='mr-5'>table 높이</label>
+            <input name='table-height' type="number" className='p-2' value={tableHeight} onChange={(event) => setTableHeight(event.target.value)}/>
+            </div>
+
+            <div>
+            <label htmlFor="table-width" className='mr-5'>table 너비</label>
+            <input name='table-width' type="number" value={tableWidth} className='p-2' onChange={(event) => setTableWidth(event.target.value)}/>
+            </div>
+
+            <div>
+            <label htmlFor="td-padding" className='mr-5'>table border</label>
+            <input name='td-padding' type="number" value={tableBorder} className='p-2' step={0.5} min={0} onChange={(event) => setTableBorder(parseFloat(event.target.value))}/>
+            </div>
+
+            <div>
+            <label htmlFor="td-padding" className='mr-5'>td 패딩</label>
+            <input name='td-padding' type="number" value={tdPadding} className='p-2' step={0.5} min={0} onChange={(event) => setTdPadding(parseFloat(event.target.value))}/>
+            </div>
+          </div>
+        
+        </div>
+
+      </div>
+   
+        {/* 오른쪽 */}
+        <div className="w-1/2 mx-1 py-5 grid grid-rows-2 gap-2">
+          {/* 렌더링된 html 테이블 */}
+          <div id='rendered-table' className={`w-${divHeight} h-${divWidth} bg-white text-black flex justify-center items-center 
+          p-${divPadding} font-${divFont} font-${divFontWeight} text-${divFontSize}`}
+          dangerouslySetInnerHTML={{__html: html}} />
+
+          {/* 렌더링된 이미지 프리뷰 */}
+          <div className="rendered-table-preview">
+            <img ref={previewImageRef} src="" alt="preview" />
+          </div>
+
+          {/* 원본 이미지 */}
+          {/* <div>
+            <img ref={originImgRef} src="" alt="원본 이미지" />
+          </div> */}
         </div>
         
-        <div className="w-1/2 mx-1 py-5 grid grid-rows-2">
-          {/* 원본 이미지 */}
-          <img ref={originImgRef} src="" alt="원본 이미지" />
-          {/* 렌더링된 html 테이블 */}
-          <div dangerouslySetInnerHTML={{__html: html}} className='font-medium'/>
         </div>
 
-      </div>
-
-      {/* 체크할 사항들 */}
-      <h1 className='m-5 font-bold text-3xl'>Check (GPT가 생성한 답변 기준!)</h1>
-      <div className="grid grid-cols-3">
-     
-      <BoolTypeQuestion questionText='구조 다 맞나요?' state={structCorrect} setState={setStruectCorrect} />
-      <BoolTypeQuestion questionText='글자 다 맞나요?' state={charCorrect} setState={setCharCorrect} />
-      <BoolTypeQuestion questionText='테이블 헤더(th) 구분 여부' state={thUsed} setState={setThUsed} />
-      <BoolTypeQuestion questionText='값이 빈 셀 있나요?(좌상단 헤더 제외)' state={valueEmptyCell} setState={setValueEmptyCell} />
       
-      <ListTypeQuestion questionText='특수문자, 수식 표현' answerList={['해당 없음', '유니코드', 'Latex', 'html 태그, 개체로 표현']} state={specialChar} setState={setSpecialChar} />
-      <ListTypeQuestion questionText='셀 안에 소제목' answerList={['해당 없음', '소제목도 하나의 셀 안에 표현', '소제목을 별도 셀로 표현']} state={cellSubtitle} setState={setCellSubtile} />
-      <ListTypeQuestion questionText='의미 상 병합 셀' answerList={['해당 없음', '보이는 그대로 빈 셀로 표현', '값을 중복되게 채워서 표현', '병합 셀로 표현']} state={semanticMergedCell} setState={setSemanticMergedCell} />
-      <ListTypeQuestion questionText='partial lined' answerList={['해당 없음', '모두 하나의 셀 안에 표현', '별도의 셀로 나눔']} state={partialLined} setState={setPartialLined} />
-      <ListTypeQuestion questionText='좌상단 헤더' answerList={['해당 없음', '병합된 셀로 표현', '별도의 셀로 나눔']} state={topleftHeader} setState={setTopleftHeader} />
 
-      </div>
-
-      <hr className='border-gray-500'/>
-
+      {/* 저장 버튼 */}
       <div className="m-5 flex justify-center">
           <button className="my-3 p-2 border-2 w-1/3 rounded-md text-2xl hover:bg-green-700"
-            onClick={() => {
-              saveLabel(labelId, html, structCorrect, charCorrect,
-                        thUsed, valueEmptyCell, specialChar, 
-                        cellSubtitle, semanticMergedCell, partialLined, topleftHeader)
-
-              setStruectCorrect(false)
-              setCharCorrect(false)
-              setThUsed(true)
-              setValueEmptyCell(false)
-              setSpecialChar(new Set([0]))
-              setCellSubtile(new Set([0]))
-              setSemanticMergedCell(new Set([0]))
-              setPartialLined(new Set([0]))
-              setTopleftHeader(new Set([0]))
-
-              setIsInspected(true)
+            onClick={async () => {
+              const renderedTableDOM = document.querySelector<HTMLElement>("#rendered-table")
+              if (renderedTableDOM) {
+                const canvas = await html2canvas(renderedTableDOM)
+                const imageFile = canvas.toDataURL("image/png", 1.5);
+                console.log(canvas)
+                console.log(imageFile)
+              }
             }}
-          >저장</button>
+            >저장</button>
       </div>
     
     </>
   )
 }
+
 
 export default App
